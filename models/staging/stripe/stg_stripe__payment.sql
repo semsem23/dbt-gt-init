9d619d63054b3208a21db491246b950d9c44cf82
Sami Nouari <sami.nouari@gmail.com>
2026-08-04 09:14:39 +0000

    added singular test stg_stripe__payment

--- models/staging/stripe/stg_stripe__payment.sql
+++ models/staging/stripe/stg_stripe__payment.sql
@@ -1,13 +1,9 @@
 with 
-
 source as (
-
     select * from {{ source('stripe', 'payment') }}
-
 ),
 
 renamed as (
-
     select
         id as payment_id,
         orderid as order_id,
@@ -16,9 +12,7 @@ renamed as (
         amount as payment_amount,
         created as payment_created,
         _batched_at
-
     from source
-
 )
 
 select * from renamed
